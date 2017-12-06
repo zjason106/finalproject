de02b02aa0db51747234ee939ed2dc949469c09d
jason zheng <zjason106@gmail.com>
2017-12-06 11:05:37 -0500

    Initial commit

---
+++ finalproject.cpp
@@ -0,0 +1,25 @@
+//resources:
+//    food
+//    water
+//    wood(heat)
+
+#include <iostream>
+
+
+using namespace std;
+
+int main() //introduction and first function
+{
+    char start;
+cout<<"  _____ __ __  ____  __ __  ____  __ __   ____  _          ____ _____ _       ____  ____   ___   \n";
+cout<<" | ___||  |  ||   |||  |  ||    ||  |  | |    || |        |    | ___|| |     |    ||    | |   |  \n";
+cout<<"|   |_ |  |  ||  |  |  |  | |  | |  |  ||  o  || |         |  |   |_ | |    |  o  ||  _  ||    | \n";
+cout<<" |__  ||  |  ||   | |  |  | |  | |  |  ||     || |___      |  ||__  || |___ |     ||  |  ||  D  |\n";
+cout<<" |  | ||  :  ||    ||  :  | |  | |  :  ||  _  ||     |     |  ||  | ||     ||  _  ||  |  ||     |\n";
+cout<<" |    ||     ||  .  ||   |  |  |  |   | |  |  ||     |     |  ||    ||     ||  |  ||  |  ||    _|\n";
+cout<<"  |___| |__,_||__||_| |_|  |____|  |_|  |__|__||_____|    |____||___||_____||__|__||__|__||____|\n\n\n";
+
+cout<<"               Welcome to Survival Island! Type any letter to start. \n\n                                          ";
+cin>>start;
+cout<<"         Out of all the letters you could have chosen. Why you choose "<<start<<"?"<<endl;
+}
